933103c6f50c25517b35974bc07b508dffad3873
Martín Lombardo <mlomb@users.noreply.github.com>
2019-07-19 00:36:14 -0300

    FromHSL

--- Code/include/OE/Math/Color.hpp
+++ Code/include/OE/Math/Color.hpp
@@ -1,10 +1,15 @@
-#ifndef MATH_COLOR_HPP
+﻿#ifndef MATH_COLOR_HPP
 #define MATH_COLOR_HPP
 
 #include "OE/Math/Vec4.hpp"
 
 namespace OrbitEngine { namespace Math {
 	typedef Vec4f Color;
+
+	// h ∈ [0, 360]
+	// s ∈ [0, 1]
+	// l ∈ [0, 1]
+	Color FromHSL(int h, float s, float l);
 } }
 
 #endif
---
+++ Code/src/OE/Math/Color.cpp
@@ -0,0 +1,30 @@
+#include "OE/Math/Color.hpp"
+
+namespace OrbitEngine { namespace Math {
+
+	float HueToRGB(float p, float q, float t) {
+		if (t < 0.0f) t += 1.0f;
+		if (t > 1.0f) t -= 1.0f;
+		if (t < 1.0f / 6.0f) return p + (q - p) * 6.0f * t;
+		if (t < 1.0f / 2.0f) return q;
+		if (t < 2.0f / 3.0f) return p + (q - p) * (2.0f / 3.0f - t) * 6.0f;
+		return p;
+	}
+
+	Color FromHSL(int h, float s, float l) {
+		if (s < 1e-5) { // achromatic
+			return Color(l, l, l, 1.0f);
+		}
+
+		float q = l < 0.5f ? l * (1.0f + s) : l + s - l * s;
+		float p = 2.0f * l - q;
+
+		return Color(
+			HueToRGB(p, q, h + 1.0f / 3.0f),
+			HueToRGB(p, q, h),
+			HueToRGB(p, q, h - 1.0f / 3.0f),
+			1.0f
+		);
+	}
+
+} }
