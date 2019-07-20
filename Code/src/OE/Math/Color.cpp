b64b7ed6d0e01d8e77d1519babccaaba7702a85f
Martín Lombardo <mlomb@users.noreply.github.com>
2019-07-19 23:35:52 -0300

    Fixed a bug in FromHSL

--- Code/src/OE/Math/Color.cpp
+++ Code/src/OE/Math/Color.cpp
@@ -18,11 +18,12 @@ namespace OrbitEngine { namespace Math {
 
 		float q = l < 0.5f ? l * (1.0f + s) : l + s - l * s;
 		float p = 2.0f * l - q;
+		float r = (float)h / 360.0f;
 
 		return Color(
-			HueToRGB(p, q, h + 1.0f / 3.0f),
-			HueToRGB(p, q, h),
-			HueToRGB(p, q, h - 1.0f / 3.0f),
+			HueToRGB(p, q, r + (1.0f / 3.0f)),
+			HueToRGB(p, q, r),
+			HueToRGB(p, q, r - (1.0f / 3.0f)),
 			1.0f
 		);
 	}
