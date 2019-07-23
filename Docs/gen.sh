16f734194bfe020f8e42bf3a674d3849e3d5c3d9
Martín Lombardo <mlomb@users.noreply.github.com>
2019-07-23 20:36:59 -0300

    initial Doxygen setup

---
+++ Docs/gen.sh
@@ -0,0 +1,6 @@
+sudo pip3 install jinja2 Pygments
+sudo apt install texlive-base texlive-latex-extra texlive-fonts-extra texlive-fonts-recommended
+
+git clone git://github.com/mosra/m.css
+cd m.css/documentation
+py ./doxygen.py ../../Doxyfile
