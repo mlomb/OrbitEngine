504fe88671897e8715947436e97311911df5e661
Martín Lombardo <mlomb@users.noreply.github.com>
2019-07-23 20:41:38 -0300

    removed sudo from docs/gen.sh

--- Docs/gen.sh
+++ Docs/gen.sh
@@ -1,5 +1,5 @@
-sudo pip3 install jinja2 Pygments
-sudo apt install texlive-base texlive-latex-extra texlive-fonts-extra texlive-fonts-recommended
+pip3 install jinja2 Pygments
+apt install texlive-base texlive-latex-extra texlive-fonts-extra texlive-fonts-recommended
 
 git clone git://github.com/mosra/m.css
 cd m.css/documentation
