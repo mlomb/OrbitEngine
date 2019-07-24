0320330bfb916c04f6ba3bac4d9e4071459d414b
Martín Lombardo <mlomb@users.noreply.github.com>
2019-07-23 22:00:29 -0300

    docs requirements.txt

--- Docs/gen.sh
+++ Docs/gen.sh
@@ -1,5 +1,4 @@
-pip3 install python3-pip jinja2 Pygments
-apt install texlive-base texlive-latex-extra texlive-fonts-extra texlive-fonts-recommended
+#apt install texlive-base texlive-latex-extra texlive-fonts-extra texlive-fonts-recommended
 
 git clone git://github.com/mosra/m.css
 cd m.css/documentation
