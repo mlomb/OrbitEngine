aa0a9f853f1f26ce15d0a5f82da6addb6663d6a2
Martín Lombardo <mlomb@users.noreply.github.com>
2019-07-29 13:56:23 -0300

    copy images directly to the output

--- Docs/gen.sh
+++ Docs/gen.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# copy images directly to the output
+cp -a /img/. /output/html/
+
 git clone git://github.com/mosra/m.css
 cd m.css/documentation
 ./doxygen.py ../../Doxyfile
