1a43d7b0f0addce46525d7f9be693e0580f959d1
Martín Lombardo <mlomb@users.noreply.github.com>
2019-08-02 00:28:44 -0300

    configure

--- Docs/gen.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-git clone git://github.com/mosra/m.css
-cd m.css/documentation
-./doxygen.py ../../Doxyfile
-
-# copy images directly to the output
-cp -a ./img/. ./output/html/
