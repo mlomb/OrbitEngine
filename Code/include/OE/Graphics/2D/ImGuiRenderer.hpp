c11143d9dac464d9a2ebe2a1f72dfe29a934b389
Martín Lombardo <mlomb@users.noreply.github.com>
2019-08-06 20:27:48 -0300

    macro to allow easy conversion between ImVec2 and Vec2f

--- Code/include/OE/Graphics/2D/ImGuiRenderer.hpp
+++ Code/include/OE/Graphics/2D/ImGuiRenderer.hpp
@@ -3,6 +3,11 @@
 
 #include "OE/Graphics/API/Mesh.hpp"
 #include "OE/Graphics/2D/Renderer2D.hpp"
+
+#define IM_VEC2_CLASS_EXTRA \
+	ImVec2(const OrbitEngine::Math::Vec2f& v) : x(v.x), y(v.y) { } \
+	operator OrbitEngine::Math::Vec2f() const { return OrbitEngine::Math::Vec2f(x, y); }
+
 #include "imgui.h"
 #include "misc/cpp/imgui_stdlib.h"
 
