85a1ba881fc1af86d892f3b91bf69761cd7765a2
Martín Lombardo <mlomb@users.noreply.github.com>
2019-08-09 15:14:02 -0300

    Gradient, ColorGradient

---
+++ Code/include/OE/Math/ColorGradient.hpp
@@ -0,0 +1,13 @@
+#ifndef MATH_COLOR_GRADIENT_HPP
+#define MATH_COLOR_GRADIENT_HPP
+
+#include "OE/Math/Color.hpp"
+#include "OE/Math/Gradient.hpp"
+
+namespace OrbitEngine { namespace Math {
+	
+	typedef Gradient<Color> ColorGradient;
+
+} }
+
+#endif
---
+++ Code/include/OE/Math/Gradient.hpp
@@ -0,0 +1,72 @@
+﻿#ifndef MATH_GRADIENT_HPP
+#define MATH_GRADIENT_HPP
+
+#include <vector>
+
+#include "OE/Math/Math.hpp"
+
+namespace OrbitEngine { namespace Math {
+
+	template<typename T> class Gradient;
+
+	/// Value stop within a Gradient
+	template<typename T>
+	class GradientStop {
+	public:
+		GradientStop(float time, const T& value)
+			: time(time), value(value) { }
+
+	private:
+		friend class Gradient<T>;
+
+		float time;
+		T value;
+	};
+
+	/// Gradient used for animations.
+	/// time ∈ [0, 1]
+	template<typename T>
+	class Gradient {
+	public:
+		/// Remove all stops
+		void reset() {
+			m_Stops.clear();
+		}
+
+		/// Add a stop
+		void addStop(float time, const T& value) {
+			auto it = m_Stops.begin();
+			for (; it != m_Stops.end(); it++)
+				if ((*it).time > time) break;
+			m_Stops.insert(it, GradientStop<T>(time, value));
+		}
+
+		/// Evaluate the gradient
+		T evaluate(float time) {
+			GradientStop<T> *start = NULL, *stop = NULL;
+
+			auto it = m_Stops.begin();
+			for (; it != m_Stops.end(); it++) {
+				stop = &(*it);
+				if (stop->time > time)
+					break;
+			}
+			if (it == m_Stops.begin() || it == m_Stops.end()) {
+				if (stop == NULL)
+					return T();
+				else
+					return stop->value;
+			}
+			start = &(*(--it));
+			float frac = (time - start->time) / (stop->time - start->time);
+			return lerp(start->value, stop->value, frac);
+		}
+
+	private:
+		std::vector<GradientStop<T>> m_Stops;
+	};
+
+
+} }
+
+#endif
