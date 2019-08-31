dc541707aa9566c4cfb5deccf72f51f63e0d3a81
Martín Lombardo <mlomb@users.noreply.github.com>
2019-08-31 12:38:37 -0300

    UVs union array

--- Code/include/OE/Math/UV.hpp
+++ Code/include/OE/Math/UV.hpp
@@ -6,13 +6,16 @@
 namespace OrbitEngine { namespace Math {
 	/// Describes the uv coordinates of a single quad
 	struct UV {
-		UV() = default;
+		UV() { };
 		UV(const float u0, const float v0, const float u1, const float v1)
 			: _1(Vec2f(u0, v0)), _2(Vec2f(u0, v1)), _3(Vec2f(u1, v1)), _4(Vec2f(u1, v0)) { };
 		UV(const Vec2f& _1, const Vec2f& _2, const Vec2f& _3, const Vec2f& _4)
 			: _1(_1), _2(_2), _3(_3), _4(_4) { };
 
-		Vec2f _1, _2, _3, _4;
+		union {
+			Vec2f arr[4];
+			struct { Vec2f _1, _2, _3, _4; };
+		};
 
 		/// Get UV coordinates by index
 		Vec2f getUV(int pos) const {
