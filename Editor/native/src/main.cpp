a48cec10a3783898291fca239063e65b2bd2fae6
Martín Lombardo <mlomb@users.noreply.github.com>
2020-02-17 00:47:59 -0300

    basic IPC test

--- Editor/native/src/main.cpp
+++ Editor/native/src/main.cpp
@@ -1,7 +1,93 @@
 
 #include <iostream>
 
+#include "windows.h"
+#include <WinUser.h>
+
+#include <OE/Application/Window.hpp>
+#include <OE/Application/Looper.hpp>
+
+using namespace OrbitEngine;
+using namespace OrbitEngine::Application;
+using namespace OrbitEngine::Misc;
+
+HANDLE hPipe;
+int image_width = 1920;
+int image_height = 1080;
+int image_size = 4 * image_width * image_height;
+unsigned char* image = NULL;
+
+class Service : public Loopeable {
+public:
+	void render() {
+		if (!image) {
+			image = (unsigned char*)malloc(image_size);
+
+			for (int x = 0; x < image_width; x++) {
+				for (int y = 0; y < image_height; y++) {
+					int p = (y * image_width + x) * 4;
+					image[p + 0] = 255;
+					image[p + 1] = 0;
+					image[p + 2] = 0;
+					image[p + 3] = 255;
+				}
+			}
+		}
+
+		BOOL bResult;
+		DWORD cbBytes;
+		bResult = WriteFile(
+			hPipe,
+			image,
+			image_size,
+			&cbBytes,
+			NULL);
+	}
+};
+
+#define BUFFER_SIZE 1024 * 1024 * 20
+
 int main() {
-    printf("Hello World!\n");
+	hPipe = CreateNamedPipe(
+		"\\\\.\\pipe\\OrbitEngineEditorService",             // pipe name 
+		PIPE_ACCESS_DUPLEX,       // read/write access 
+		PIPE_TYPE_MESSAGE |       // message type pipe 
+		PIPE_READMODE_MESSAGE   // message-read mode 
+		/* | PIPE_WAIT*/,                // blocking mode 
+		PIPE_UNLIMITED_INSTANCES, // max. instances  
+		BUFFER_SIZE,              // output buffer size 
+		BUFFER_SIZE,              // input buffer size 
+		NMPWAIT_USE_DEFAULT_WAIT, // client time-out 
+		NULL);                    // default security attribute 
+
+	if (INVALID_HANDLE_VALUE == hPipe)
+	{
+		printf("\nError occurred while "
+			"creating the pipe: %d", GetLastError());
+		fflush(stdout);
+		return 1;  //Error
+	}
+
+	//Wait for the client to connect
+	BOOL bClientConnected = ConnectNamedPipe(hPipe, NULL);
+
+	if (FALSE == bClientConnected)
+	{
+		printf("\nError occurred while connecting"
+			" to the client: %d", GetLastError());
+		fflush(stdout);
+		CloseHandle(hPipe);
+		return 1;  //Error
+	}
+
+	Service* service = new Service();
+
+	Window* w = new Window();
+	Context* context = new Context(RenderAPI::OPENGL, w);
+	Looper* looper = new Looper(context, service);
+	looper->loop();
+
+	CloseHandle(hPipe);
+
     return 0;
 }
--- Editor/src/service/EngineService.js
+++ Editor/src/service/EngineService.js
@@ -1,22 +1,43 @@
-import net from 'net';
+import { Socket } from 'net';
 import fs from 'fs';
 import path from 'path';
 import { spawn } from 'child_process';
 import { EventEmitter } from 'events';
 import { remote } from 'electron';
 
+import { isDevMode } from '../ApplicationContext';
+
 class EngineService extends EventEmitter {
     constructor() {
         super();
+
+        // Note: Only Windows for now
+        // TODO: name should contain a hash to allow multiple instances of the engine
+        const name = 'OrbitEngineEditorService';
+        this.ipcPath = `\\\\.\\pipe\\${name}`;
+
+        this.ipcService = new Socket();
+        this.ipcService.on('connect', () => {
+            console.log("Connected via IPC to the Engine Service");
+            this.emit('connected');
+        });
+        this.ipcService.on('error', (err) => {
+            console.log(err);
+            this.emit('disconnected');
+        });
+        this.ipcService.on('data', this.onData.bind(this));
     }
 
     boot() {
-        console.log("Booting");
+        if(!isDevMode)
+            this.spawnService();
+        this.initConnection();
+    }
+
+    spawnService() {
         const ls = spawn(path.join(remote.app.getAppPath(), 'native/EditorService.exe').replace('app.asar', 'app.asar.unpacked'), {
             cwd: __dirname,
-            detached: false,
-            /*shell: false,
-            windowsHide: true*/
+            detached: false
         });
 
         ls.stdout.on('data', (data) => {
@@ -34,6 +55,14 @@ class EngineService extends EventEmitter {
         ls.on('error', (err) => {
             console.log(err);
         });
+    }
+
+    initConnection() {
+        this.ipcService.connect(this.ipcPath);
+    }
+
+    onData(data) {
+        console.log("DATA", data.length);
     }
 }
 
