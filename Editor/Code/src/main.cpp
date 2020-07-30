e9d88a65cbb162a947d09268240d5a17143f5e62
Martín Lombardo <mlombardo9@gmail.com>
2020-07-29 22:32:12 -0300

    Moved old UI

--- Editor/Code/src/main.cpp
+++ Editor/Code/src/main.cpp
@@ -1,50 +1,30 @@
-#include <stdio.h>
 
-#include "Cef.hpp"
-#include "CefController.hpp"
+#include <OE/Application/Window.hpp>
+#include <OE/Application/Context.hpp>
+#include <OE/Misc/Ticker.hpp>
 
-#include "EngineEditor.hpp"
-
-#include <OE/Misc/Log.hpp>
-
-int main(int argc, char* argv[]) {
+int main() {
 	using namespace OrbitEngine;
-	using namespace OrbitEngine::Editor;
+	using namespace OrbitEngine::Application;
+	using namespace OrbitEngine::Misc;
 
-	CefEnableHighDPISupport();
+	Window* window = new Window();
+	window->setTitle("UI Testing");
+	window->setVisibility(true);
+	
+	Context* ctx = new Context(RenderAPI::OPENGL, window);
 
-	#if OE_WINDOWS
-	CefMainArgs args(GetModuleHandle(NULL));
-	#else
-	CefMainArgs args(argc, argv);
-	#endif
+	Ticker t;
 
-	CefRefPtr<CefController> controller = new CefController();
+	while (!window->destroyRequested()) {
+		window->processEvents();
+		t.tick();
+		ctx->prepare();
 
-	int exit_code = CefExecuteProcess(args, controller, nullptr);
-	if (exit_code >= 0)
-		return exit_code;
+		// render
 
-	OE_LOG_DEBUG("CEF " << CEF_VERSION);
-
-	CefSettings settings;
-	settings.windowless_rendering_enabled = true;
-	settings.multi_threaded_message_loop = false;
-#if OE_DEBUG
-	settings.remote_debugging_port = 9222;
-#endif
-
-	if (!CefInitialize(args, settings, controller, NULL)) {
-		OE_LOG_FATAL_SHOWBOX_AND_EXIT("Failed to initialize CEF");
-		// unreachable
+		ctx->present();
 	}
-
-	{
-		EngineEditor editor;
-		editor.run();
-	}
-
-	CefShutdown();
 
 	return 0;
 }
